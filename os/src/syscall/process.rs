9b7e2d946d69e14ffb8498b20b30456ebb037b8a
Murrrphy <444575716@qq.com>
2025-05-11 16:24:32 +0800

    Revert "移植get_time，未考虑跨页问题"
    This reverts commit 3fee51adcd1b99e1e28c63dbda4216b851372ca1.

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,10 +1,10 @@
 use crate::{
     fs::{open_file, OpenFlags},
-    mm::{translated_byte_buffer, translated_ref, translated_refmut, translated_str},
+    mm::{translated_ref, translated_refmut, translated_str},
     task::{
         current_process, current_task, current_user_token, exit_current_and_run_next, pid2process,
         suspend_current_and_run_next, SignalFlags,
-    }, timer::get_time_us,
+    },
 };
 use alloc::{string::String, sync::Arc, vec::Vec};
 
@@ -151,23 +151,12 @@ pub fn sys_kill(pid: usize, signal: u32) -> isize {
 /// YOUR JOB: get time with second and microsecond
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
-pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
+pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
     trace!(
-        "kernel:pid[{}] sys_get_time",
+        "kernel:pid[{}] sys_get_time NOT IMPLEMENTED",
         current_task().unwrap().process.upgrade().unwrap().getpid()
     );
-    let usec = get_time_us();
-    let sec = usec * 1_000_000;
-    let time = TimeVal { sec, usec };
-
-    let token = current_user_token();
-    let mut buffers = translated_byte_buffer(token, ts as *const _ as *const u8, core::mem::size_of::<TimeVal>());
-    let src = unsafe {
-        core::slice::from_raw_parts(&time as *const _ as *const u8, core::mem::size_of::<TimeVal>())
-    };
-    buffers[0].copy_from_slice(src);
-
-    0
+    -1
 }
 
 /// mmap syscall
